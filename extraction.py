bb3ba6f8f57d6ba23aadd94c4db5f2bd209278f1
benitesf <edzon_bf@hotmail.com>
2021-01-03 13:37:45 +0100

    add more feats

--- extraction.py
+++ extraction.py
@@ -6,14 +6,17 @@ Created on Wed Dec 30 14:40:54 2020
 @author: joker
 """
 
+import os
+import datetime
 import pandas as pd
 import _pickle as pkl
+import seaborn as sns
 
 import filter
-from variables import *
+import macros
 
 data = []
-filename = "sale.data"
+filename = os.path.join(os.getcwd(), "data", "raw", "sale_31-12-2020.data")
 with open(filename, 'rb') as fr:
     try:
         while True:
@@ -22,25 +25,33 @@ with open(filename, 'rb') as fr:
         pass
     
 
-cols = ["id_urbania", "date", "sell_price",
-        "rental_price", "operation", "property",
-        "province", "city", "neighborhood",
-        "antiquity", "bathroom", "toilet", "bedroom",
-        "covered_area", "total_area", "garage"]
+cols = ["id_urbania", "id_publisher", "date", "title", "description",
+        "sell_price", "rental_price", "operation", "property",
+        "province_id", "province", "city_id", "city", "neighborhood_id",
+        "neighborhood", "address", "antiquity", "bathroom", "toilet",
+        "bedroom", "covered_area", "total_area", "garage", "url"]
 df = pd.DataFrame()
 
 
 for chunk in data:
     for row in chunk:
         id_urbania = row["id"]["id_urb"]
+        id_publisher = row["id"]["id_pub"]
         date = row["date"]
+        title = row["title"]
+        description = row["desc"]
         sell_price = row["price"]["sell_price"]
         rental_price = row["price"]["rental_price"]
         operation = row["oper"]
         property_ = row["prop"]
+        province_id = row["loc"]["prov"]["id"]
         province = row["loc"]["prov"]["value"]
+        city_id = row["loc"]["city"]["id"]
         city = row["loc"]["city"]["value"]
+        neighborhood_id = row["loc"]["neigh"]["id"]
         neighborhood = row["loc"]["neigh"]["value"]
+        address = row["loc"]["addr"]
+        url = row["url"]
         
         antiquity = ''
         bathroom = ''
@@ -66,20 +77,24 @@ for chunk in data:
         if "icon-cochera" in feats:
             garage = feats["icon-cochera"]
 
-        s = pd.Series([id_urbania, date, sell_price, rental_price, operation,
-                       property_, province, city, neighborhood, antiquity,
-                       bathroom, toilet, bedroom, covered_area, total_area, garage])
+        s = pd.Series([id_urbania, id_publisher, date, title, description,
+                       sell_price, rental_price, operation, property_,
+                       province_id, province, city_id, city, neighborhood_id,
+                       neighborhood, address, antiquity, bathroom, toilet, 
+                       bedroom, covered_area, total_area, garage, url])
         row_df = pd.DataFrame([s])
         df = pd.concat([row_df, df], ignore_index=True)
 
+
 df.columns = cols
+now = datetime.datetime.now()
+#df_f = df.copy()
 
-df_f = df.copy()
-
-# Filter Data
+# Clean and filter data
 df = filter.clean_single_quote(df)
+df = filter.clean_double_quote(df)
 df = filter.strip(df)
-df["date"] = df["date"].apply(filter.calculate_date)
+df["date"] = df["date"].apply(filter.calculate_date, args=(now,))
 df["sell_price"] = df["sell_price"].apply(filter.extract_only_number)
 df["rental_price"] = df["rental_price"].apply(filter.extract_only_number)
 df["antiquity"] = df["antiquity"].apply(filter.clean_antiquity)
@@ -91,10 +106,21 @@ df["total_area"] = df["total_area"].apply(filter.extract_only_number)
 df["garage"] = df["garage"].apply(filter.extract_only_number)
 
 # Set types
-df = df.astype({"id_urbania": str, "date": str, "sell_price": int,
+df = df.astype({"id_urbania": str, "id_publisher": str, "date": str,
+                "title": str, "description": str, "sell_price": int,
                 "rental_price": int, "operation": str, "property": str,
-                "province": str, "city": str, "neighborhood": str,
-                "antiquity": int, "bathroom": int, "toilet": int,
-                "bedroom": int, "covered_area": int, "total_area": int,
-                "garage": int
+                "province_id": str, "province": str, "city_id": str,
+                "city": str, "neighborhood_id": str, "neighborhood": str,
+                "address": str, "antiquity": int, "bathroom": int,
+                "toilet": int, "bedroom": int, "covered_area": int,
+                "total_area": int, "garage": int, "url": str
                 })
+
+to_filename = os.path.join(os.getcwd(), "data", "extracted", "sale_extracted_31-12-2020.data")
+df.to_pickle(to_filename)
+
+"""
+lima_df = df[df["province"] == "Lima"][["date", "sell_price", "antiquity",
+                                        "bathroom", "toilet", "bedroom",
+                                        "covered_area", "total_area", "garage"]]
+"""
